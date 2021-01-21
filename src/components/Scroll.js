90c3d938d2354d55297d67fc1416ba22a32e13a1
Luis Abrego <luis.abrego@gmail.com>
2021-01-21 14:04:52 -0600

    Added Github page deployment

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,7 @@
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
+        <div style={{ overflowY: 'scroll', border: '0px solid black', height: '800px'}}>
             {props.children}
         </div>
     );
